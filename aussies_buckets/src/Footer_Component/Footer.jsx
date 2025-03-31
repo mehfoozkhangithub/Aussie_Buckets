0c9f1653d0656b37624109b6926ea3efe0198f50
rngfires <rohangawade2604@gmail.com>
2025-03-31 11:07:08 +0530

    just done with the widht part

--- aussies_buckets/src/Footer_Component/Footer.jsx
+++ aussies_buckets/src/Footer_Component/Footer.jsx
@@ -4,7 +4,7 @@ import footie from "../assets/Footer.png"
 const Footer = () => {
     return (
         <>
-            <div className='bg-black w-100% h-500 text-white'  >
+            <div className='bg-black w-full h-500 text-white mr-10 '  >
 
                 <div className="part pt-25 w-100%  flex justify-evenly">
 
