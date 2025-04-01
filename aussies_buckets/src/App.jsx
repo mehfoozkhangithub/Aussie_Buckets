7c44e977abdd96e9fc3dea8f03f481eef265e6c1
rohan03-stack <ronygaikwad03@gmail.com>
2025-04-01 23:57:28 +0530

    card added in testimonial

--- aussies_buckets/src/App.jsx
+++ aussies_buckets/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Navbar } from './Nav/Navbar'
+import { Testimonial } from './Pages/Testimonial'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
       <Navbar />
+
+      <Testimonial />
     </>
   )
 }
