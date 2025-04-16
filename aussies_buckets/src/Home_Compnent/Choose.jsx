631e63d9ba71cf9d28f00ef6e834b945c03fc525
rohangawade-2604 <rohangawade2604@gmail.comm>
2025-04-17 01:55:37 +0530

    done with some errors

--- aussies_buckets/src/Home_Compnent/Choose.jsx
+++ aussies_buckets/src/Home_Compnent/Choose.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+
+
+
 
 export const Choose = () => {
+
+    const Card = [
+        {
+            id: 1,
+            img: "../assets/ILS.png",
+            h1: "Industry-Leading Support",
+            p: "Our team is here to assist you, from selecting the right attachment to after-sales service. We offer lifelong after-sales support, including technical troubleshooting and general product care."
+               
+        },
+
+        {
+            id: 1,
+            img: "../assets/FSAA.png",
+            h1: "Industry-Leading Support",
+            p: "Our team is here to assist you, from selecting the right attachment to after-sales service. We offer lifelong after-sales support, including technical troubleshooting and general product care."
+               
+        },
+
+        {
+            id: 1,
+            img: "../assets/ET.png",
+            h1: "Industry-Leading Support",
+            p: "Our team is here to assist you, from selecting the right attachment to after-sales service. We offer lifelong after-sales support, including technical troubleshooting and general product care."
+               
+        },
+
+        {
+            id: 1,
+            img: "../assets/CS.png",
+            h1: "Industry-Leading Support",
+            p: "Our team is here to assist you, from selecting the right attachment to after-sales service. We offer lifelong after-sales support, including technical troubleshooting and general product care."
+               
+        }
+    ]
+
+
   return (
     <>
         <div className="choose overflow-hidden h-300">
@@ -12,6 +49,18 @@ export const Choose = () => {
 
                 <img src="https://cdn.prod.website-files.com/66b04d5d579de4c891b7d35c/6799640b458240f817a8d6ec_02_Team-02.avif" alt="" className='h-50 w-350 object-cover ' />
             </div>
+
+        <div className="cards">
+            {
+                Card.map((el , id) => (
+                    <div key={id}>
+                        <img src={el.img} alt="" />
+                        <h1>{el.h1}</h1>
+                        <p>{el.p}</p>
+                    </div>
+                ))
+            }
+        </div>
           
         </div>
     </>
